a714d5bc0356d575ce1bd461d15f7ad8080db055
Kenta Yamada <knt.yamada.800@gamil.com>
2020-02-09 12:15:50 +0900

    #3 add plugins

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import Vue from "vue";
+import Buefy from "buefy";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import "buefy/dist/buefy.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
+
 Vue.config.productionTip = false;
+Vue.use(Buefy, {
+  defaultIconPack: "fas"
+});
 
 new Vue({
   router,

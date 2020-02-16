59c16672c26397f67e14e52acddcc30d8287e506
Kenta Yamada <knt.yamada.800@gamil.com>
2020-02-16 12:03:10 +0900

    #4 port変更

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,14 @@
 module.exports = {
   pages: {
     index: {
-      entry: 'src/main.ts',
-      template: 'public/index.html',
-      filename: 'index.html',
+      entry: "src/main.ts",
+      template: "public/index.html",
+      filename: "index.html",
       title: "Siphon 予約管理システム",
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    }
+  },
+  devServer: {
+    port: 8081
   }
 };

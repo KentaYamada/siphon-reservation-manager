aeab042945d8c6922c1d17bb9b75c84dc2b3ac0c
Kenta Yamada <knt.yamada.800@gamil.com>
2020-02-24 14:31:54 +0900

    #14 リスト表示をPC / モバイルで切り替えて表示できるよにする

--- src/views/managements/reservation-list/script.ts
+++ src/views/managements/reservation-list/script.ts
@@ -1,11 +1,14 @@
 import Vue from "vue";
-import ReservationListItem from "@/components/reservations/reservation-list-item/ReservationListItem.vue";
+import isMobile from "ismobilejs";
+import ReservationListForPc from "@/components/reservations/reservation-list-for-pc/ReservationListForPc.vue";
+import ReservationListForMobile from "@/components/reservations/reservation-list-for-mobile/ReservationListForMobile.vue";
 import ReservationSearchForm from "@/components/reservations/reservation-search-form/ReservationSearchForm.vue";
 
 export default Vue.extend({
   components: {
     ReservationSearchForm,
-    ReservationListItem
+    ReservationListForPc,
+    ReservationListForMobile
   },
   data() {
     const reservations = [
@@ -51,7 +54,8 @@ export default Vue.extend({
       }
     ];
     return {
-      reservations
+      reservations,
+      isMobilePhone: isMobile().phone
     };
   }
 });

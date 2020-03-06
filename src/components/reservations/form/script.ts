48235425c0613a4cd41f5fc1a19061c2ea89c352
Kenta Yamada <knt.yamada.800@gamil.com>
2020-03-06 22:18:32 +0900

    #9 フォームコンポーネントへのデータ伝播実装

--- src/components/reservations/form/script.ts
+++ src/components/reservations/form/script.ts
@@ -1,5 +1,23 @@
-import Vue from "vue";
+import Vue, { PropType } from "vue";
+import { mapActions, mapState } from "vuex";
+import { FETCH } from "@/store/constant";
+import { Reservation } from "@/entity/reservation";
 
 export default Vue.extend({
-  template: "<reservation-form/>"
+  template: "<reservation-form/>",
+  props: {
+    reservation: {
+      required: true,
+      type: Object as PropType<Reservation>
+    }
+  },
+  computed: {
+    ...mapState("timezone", ["timezones"])
+  },
+  methods: {
+    ...mapActions("timezone", [FETCH])
+  },
+  mounted() {
+    this.fetch();
+  }
 });
--- src/views/reservations/entry/script.ts
+++ src/views/reservations/entry/script.ts
@@ -1,8 +1,19 @@
 import Vue from "vue";
+import { mapMutations, mapState } from "vuex";
+import { INITIALIZE } from "@/store/constant";
 import ReservationForm from "@/components/reservations/form/ReservationForm.vue";
 
 export default Vue.extend({
   components: {
     ReservationForm
+  },
+  computed: {
+    ...mapState("reservation", ["reservation"])
+  },
+  methods: {
+    ...mapMutations("reservation", [INITIALIZE])
+  },
+  mounted() {
+    this.initialize();
   }
 });

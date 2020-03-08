e0b0517a7796ccf8b88babeb4593abec2d123520
Kenta Yamada <knt.yamada.800@gamil.com>
2020-03-08 21:17:35 +0900

    #32 予約変更フォームへの導線実装

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import VueRouter, { RouteConfig, Route } from "vue-router";
 // Routing URL
 import {
   INDEX_URL,
+  RESERVATION_EDIT_URL,
   RESERVATION_ENTRY_URL,
   RESERVATION_DETAIL_URL,
   RESERVED_MESSAGE_URL,
@@ -14,8 +15,9 @@ import {
 } from "@/router/url";
 
 // Reservation
-import ReservationEntry from "@/views/reservations/entry/ReservationEntry.vue";
 import ReservationDetail from "@/views/reservations/detail/ReservationDetail.vue";
+import ReservationEdit from "@/views/reservations/edit/ReservationEdit.vue";
+import ReservationEntry from "@/views/reservations/entry/ReservationEntry.vue";
 import ReservationList from "@/views/reservations/list/ReservationList.vue";
 import ReservedMessage from "@/views/reservations/reserved-message/ReservedMessage.vue";
 
@@ -40,6 +42,14 @@ const routes: RouteConfig[] = [
     path: RESERVATION_ENTRY_URL,
     name: "reservation-entry",
     component: ReservationEntry
+  },
+  {
+    path: `${RESERVATION_EDIT_URL}/:id`,
+    name: "reservation-edit",
+    component: ReservationEdit,
+    props: (router: Route) => ({
+      id: parseInt(router.params.id, 10)
+    })
   },
   {
     path: `${RESERVATION_DETAIL_URL}/:id`,
---
+++ src/views/reservations/edit/script.ts
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import { mapActions, mapState } from "vuex";
+import ReservationForm from "@/components/reservations/form/ReservationForm.vue";
+
+export default Vue.extend({
+  components: {
+    ReservationForm
+  },
+  props: {
+    id: {
+      required: true,
+      type: Number
+    }
+  },
+  computed: {
+    ...mapState("reservation", ["reservation"])
+  },
+  methods: {
+    /**
+     *  予約変更イベント
+     */
+    onClickSave(): void {
+      this.save(this.reservation);
+    }
+  },
+  data() {
+    const errors = {
+      // reservation_date: ["予約日時は必須です。", "hoge"],
+      // reservation_timezone: ["時間帯を選択してください。", "hoge"],
+      // reservation_seats: ["座席を指定してください。"],
+      // number_of_reservations: ["予約人数は1名から入力してください。"],
+      // name: ["お名前は必須入力です。"],
+      // tel: ["電話番号は必須入力です。"],
+      // mail: ["メールアドレスは必須入力です。"]
+    };
+
+    return { errors };
+  }
+});

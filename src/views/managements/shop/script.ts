c185db57e19f180780eb4f625db6fbf2bed03891
Kenta Yamada <knt.yamada.800@gamil.com>
2020-03-15 22:40:10 +0900

    #15 営業日新規作成の導線実装

--- src/views/managements/shop/script.ts
+++ src/views/managements/shop/script.ts
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import { mapActions, mapState } from "vuex";
+import { DialogConfig, ModalConfig, ToastConfig } from "buefy/types/components";
 
 // component
+import BusinessDayForm from "@/components/business-day/dialog/BusinessDayForm.vue";
 import BusinessDayList from "@/components/business-day/list/BusinessDayList.vue";
 
 // entity
@@ -27,6 +29,39 @@ export default Vue.extend({
   methods: {
     ...mapActions("shop", [FETCH]),
 
+    /**
+     * 営業日設定フォーム表示
+     */
+    handleShowBusinessDayForm(): void {
+      const businessDay: BusinessDay = {
+        business_date: new Date()
+      };
+      const config: ModalConfig = {
+        parent: this,
+        component: BusinessDayForm,
+        hasModalCard: true,
+        scroll: "keep",
+        props: {
+          businessDay: businessDay
+        },
+        events: {
+          "save-success": () => {
+            const toastConfig: ToastConfig = {
+              message: "保存しました。",
+              type: "is-success"
+            };
+
+            this.$buefy.toast.open(toastConfig);
+          }
+        }
+      };
+
+      this.$buefy.modal.open(config);
+    },
+
+    /**
+     * 追加メニューボタンのトグル
+     */
     toggleAddMenuButtons(): void {
       this.showMenuButton = !this.showMenuButton;
     }

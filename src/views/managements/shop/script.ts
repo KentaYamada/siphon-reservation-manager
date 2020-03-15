681bfaf5ddc4aa89797ff6b60923a54d5fd6bc3e
Kenta Yamada <knt.yamada.800@gamil.com>
2020-03-15 16:06:44 +0900

    #15 データ取得関連の導線実装

--- src/views/managements/shop/script.ts
+++ src/views/managements/shop/script.ts
@@ -1,4 +1,18 @@
 import Vue from "vue";
+import { mapActions, mapState } from "vuex";
+import { BusinessDay } from "@/entity/business-day";
+import { Timezone } from "@/entity/timezone";
+import { FETCH } from "@/store/constant";
+import { ShopState } from "@/store/shop";
 
 export default Vue.extend({
+  computed: {
+    ...mapState("shop", ["businessDays", "timezones"])
+  },
+  methods: {
+    ...mapActions("shop", [FETCH])
+  },
+  mounted() {
+    this.fetch();
+  }
 });

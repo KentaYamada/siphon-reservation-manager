a467b6297318b248c5d728681f5cb69b9b492f34
Kenta Yamada <knt.yamada.800@gamil.com>
2020-03-17 22:44:50 +0900

    #15 予約時間帯設定ダイアログレイアウト

--- src/components/timezones/dialog/script.ts
+++ src/components/timezones/dialog/script.ts
@@ -11,14 +11,25 @@ export default Vue.extend({
       type: Object as PropType<Timezone>
     }
   },
+  validations: {
+    timezone: {
+      start_time: {
+        required
+      },
+      end_time: {
+        required
+      }
+    }
+  },
   methods: {
     handleClickSave(): void {
-      // this.$v.touch();
-      // if (!this.$v.$invalid()) {
-      //   // todo: when firestore save succeeded
-      //   this.$emit("close");
-      //   this.$emit("save-success", "保存しました。");
-      // }
+      this.$v.$touch();
+
+      if (!this.$v.$invalid) {
+        // todo: when firestore save succeeded
+        this.$emit("close");
+        this.$emit("save-success", "保存しました。");
+      }
     }
   }
 });

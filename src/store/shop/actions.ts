46606d74a09bff921aa93544708f991c8acf1368
Kenta Yamada <knt.yamada.800@gamil.com>
2020-03-21 11:38:20 +0900

    #15 営業日取得機能実装

--- src/store/shop/actions.ts
+++ src/store/shop/actions.ts
@@ -1,4 +1,5 @@
 import { ActionTree } from "vuex";
+import _ from "lodash";
 
 // entity
 import { BusinessDay } from "@/entity/business-day";
@@ -26,21 +27,32 @@ const actions: ActionTree<ShopState, RootState> = {
   /**
    * 店舗情報取得
    */
-  [FETCH]: ({ commit }) => {
+  [FETCH]: ({ commit }, currentDate?: Date) => {
     // todo: fetch firestore
-    const businessDays: BusinessDay[] = [
-      { business_date: new Date("2020-03-14") },
-      { business_date: new Date("2020-03-21") },
-      { business_date: new Date("2020-03-28") }
-    ];
     const timezones: Timezone[] = [
       { text: "11:00 - 12:00" },
       { text: "12:30 - 13:30" },
       { text: "14:00 - 15:00" }
     ];
 
-    commit(SET_BUSINESS_DAYS, businessDays);
     commit(SET_TIMEZONES, timezones);
+
+    const db = firebase.firestore();
+    const businessDays: BusinessDay[] = [];
+
+    db.collection(BUSINESS_DAYS)
+      .get()
+      .then(query => {
+        query.forEach(doc => {
+          const item: BusinessDay = {
+            id: doc.id,
+            business_date: doc.data().business_date.toDate()
+          };
+          businessDays.push(item);
+        });
+
+        commit(SET_BUSINESS_DAYS, businessDays);
+      });
   },
 
   /**

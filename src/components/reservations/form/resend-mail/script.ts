2d5f84ba1427a97465a34f51727edc45b6cdc9a0
Kenta Yamada <knt.yamada.800@gamil.com>
2020-05-10 21:08:12 +0900

    #65 [予約メール再送フォーム] 予約日、予約時間のデータ取得ロジック実装

--- src/components/reservations/form/resend-mail/script.ts
+++ src/components/reservations/form/resend-mail/script.ts
@@ -1,18 +1,36 @@
 import Vue from "vue";
-import { mapMutations, mapState } from "vuex";
+import { mapActions, mapMutations, mapState } from "vuex";
+import { ToastConfig } from "buefy/types/components";
 
 // store
-import { INITIALIZE } from "@/store/constant";
+import { INITIALIZE, FETCH } from "@/store/constant";
 
 export default Vue.extend({
   template: "<reservation-resend-mail-form/>",
   computed: {
+    ...mapState("businessDay", ["businessDays"]),
+    ...mapState("timezone", ["timezones"]),
     ...mapState("reservationResendMail", ["reservationResendMail"])
   },
   methods: {
+    ...mapActions("businessDay", {
+      fetchBusinessDays: FETCH
+    }),
+    ...mapActions("timezone", {
+      fetchTimezones: FETCH
+    }),
     ...mapMutations("reservationResendMail", [INITIALIZE])
   },
   created() {
     this.initialize();
+
+    Promise.all([this.fetchBusinessDays, this.fetchBusinessDays]).catch(() => {
+      const toastConfig: ToastConfig = {
+        message: "データの取得に失敗しました",
+        type: "is-danger"
+      };
+
+      this.$buefy.toast.open(toastConfig);
+    });
   }
 });

64988d28702c9b14d161ee6236a89954b86c0e4b
Kenta Yamada <knt.yamada.800@gamil.com>
2020-05-10 18:47:17 +0900

    #65 [予約メール再送フォーム] フォームコンポーネント作成

---
+++ src/views/reservations/resend-mail/script.ts
@@ -0,0 +1,10 @@
+import Vue from "vue";
+
+// components
+import ReservationResendMailForm from "@/components/reservations/form/resend-mail/ReservationResendMailForm.vue";
+
+export default Vue.extend({
+  components: {
+    ReservationResendMailForm
+  }
+});

cbc586092e5f253bffa5b444da40ed174bafe33d
Kenta Yamada <knt.yamada.800@gamil.com>
2020-05-23 00:09:13 +0900

    #65 メール再送受付データ作成機能実装

--- src/store/reservation-resend-mail/actions.ts
+++ src/store/reservation-resend-mail/actions.ts
@@ -3,15 +3,32 @@ import { ActionTree } from "vuex";
 // entity
 import { ReservationResendMail } from "@/entity/reservation-resend-mail";
 
+// plugin
+import firebase from "@/plugins/firebase";
+
 // store
 import { RootState } from "@/store";
-import { SEND_MAIL } from "@/store/constant";
+import { SAVE } from "@/store/constant";
 import { ReservationResendMailState } from "@/store/reservation-resend-mail";
 
+// firebase collection name
+const RESERVATION_RESEND_MAILS = "reservation_resend_mails";
+
 const actions: ActionTree<ReservationResendMailState, RootState> = {
-  [SEND_MAIL]: async ({ commit }, model: ReservationResendMail) => {
-    // todo: call cloud function
-    console.log(model);
+  [SAVE]: async ({ commit }, model: ReservationResendMail) => {
+    const collection = firebase
+      .firestore()
+      .collection(RESERVATION_RESEND_MAILS);
+    const data = {
+      reservation_id: model.reservation_id,
+      reservation_date_id: model.reservation_date_id,
+      reservation_timezone_id: model.reservation_time_id,
+      reserver_name: model.reserver_name,
+      tel: model.tel,
+      mail: model.mail
+    };
+
+    return await collection.add(data);
   }
 };
 

39032b1cd2fb08239da03b734190e09945b508f7
Kenta Yamada <knt.yamada.800@gamil.com>
2020-05-31 23:51:15 +0900

    #61 favicon作成

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
       entry: "src/main.ts",
       template: "public/index.html",
       filename: "index.html",
-      title: "Siphon 予約管理システム",
+      title: "Cafe de Gamoyon 予約管理システム",
       chunks: ["chunk-vendors", "chunk-common", "index"]
     }
   },

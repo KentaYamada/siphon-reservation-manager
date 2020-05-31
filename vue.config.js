2b0376d3337cd9b3acd0322c2cc4c22f104262e3
Kenta Yamada <knt.yamada.800@gamil.com>
2020-06-01 01:41:22 +0900

    #81 PWAのfavicon path設定する

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,13 @@ module.exports = {
   },
   devServer: {
     port: 8081
+  },
+  pwa: {
+    iconPaths: {
+      favicon16:"images/icons/icon-16x16.png", 
+      favicon32:"images/icons/icon-32x32.png", 
+      appleTouchIcon: "images/icons/icon-152x152.png",
+      msTitleImage:"images/icons/icon-144x144.png" 
+    }
   }
 };

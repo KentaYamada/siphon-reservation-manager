9246b8de756865b1487f6cee472413e4960e8e9d
Kenta Yamada <knt.yamada.800@gamil.com>
2020-06-10 23:50:12 +0900

    #129 ログイン画面に背景画像を表示する

--- src/views/app/script.ts
+++ src/views/app/script.ts
@@ -1,10 +1,17 @@
 import Vue from "vue";
+import { mapGetters } from "vuex";
 
 // component
 import Navigation from "@/components/navigation/Navigation.vue";
 
+// store
+import { IS_SIGNED_IN } from "@/store/constant";
+
 export default Vue.extend({
   components: {
     Navigation
+  },
+  computed: {
+    ...mapGetters("auth", [IS_SIGNED_IN])
   }
 });

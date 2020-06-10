4b30c6b263248cadf1140b0e56f0addcb50659bf
Kenta Yamada <knt.yamada.800@gamil.com>
2020-06-11 00:54:16 +0900

    fix visible login background image

--- src/views/app/script.ts
+++ src/views/app/script.ts
@@ -1,17 +1,15 @@
 import Vue from "vue";
-import { mapGetters } from "vuex";
 
 // component
 import Navigation from "@/components/navigation/Navigation.vue";
-
-// store
-import { IS_SIGNED_IN } from "@/store/constant";
 
 export default Vue.extend({
   components: {
     Navigation
   },
   computed: {
-    ...mapGetters("auth", [IS_SIGNED_IN])
+    visibleLoginBg(): boolean {
+      return this.$route.name === "login";
+    }
   }
 });

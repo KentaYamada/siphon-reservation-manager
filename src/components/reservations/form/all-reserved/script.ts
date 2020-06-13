a571bfe1e7b0269b308de87eea2b4ae3cda13b71
Kenta Yamada <knt.yamada.800@gamil.com>
2020-06-13 12:48:37 +0900

    #75 作り直し

--- src/components/reservations/form/all-reserved/script.ts
+++ src/components/reservations/form/all-reserved/script.ts
@@ -14,49 +14,5 @@ import {
 } from "@/store/constant";
 
 export default Vue.extend({
-  template: "<reservation-all-reserved-form/>",
-  props: {
-    reservation: {
-      required: true,
-      type: Object as PropType<Reservation>
-    },
-    validations: {
-      required: true,
-      type: Object
-    }
-  },
-  computed: {
-    ...mapState("businessDay", ["businessDays"]),
-    ...mapState("timezone", ["timezones"]),
-    ...mapGetters("businessDay", {
-      getBusinessDayById: GET_BY_ID
-    }),
-    ...mapGetters("timezone", {
-      getTimezoneById: GET_BY_ID
-    })
-  },
-  methods: {
-    ...mapActions("businessDay", {
-      fetchBusinessDays: FETCH
-    }),
-    ...mapActions("timezone", [FETCH_ALL_RESERVED_TIMEZONES]),
-    ...mapMutations("reservation", [
-      SET_RESERVATION_DATE,
-      SET_RESERVATION_TIMEZONE
-    ]),
-
-    onChangeBusinessDay(selectedId: string): void {
-      const businessDay = this.getBusinessDayById(selectedId);
-      this.setReservationDate(businessDay.business_date);
-    },
-
-    onChangeTimezone(selectedId: string): void {
-      const timezone = this.getTimezoneById(selectedId);
-      this.setReservationTimezone(timezone);
-    }
-  },
-  mounted() {
-    this.fetchAllReservedTimezones();
-    this.fetchBusinessDays();
-  }
+  template: "<reservation-all-reserved-form/>"
 });

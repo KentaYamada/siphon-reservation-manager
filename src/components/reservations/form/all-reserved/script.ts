66443ee01567009472f8ba0c57fc034a003b0ab9
Kenta Yamada <knt.yamada.800@gamil.com>
2020-06-13 13:01:26 +0900

    #75 予約日、予約時間のデータ取得ロジック実装

--- src/components/reservations/form/all-reserved/script.ts
+++ src/components/reservations/form/all-reserved/script.ts
@@ -1,5 +1,6 @@
 import Vue, { PropType } from "vue";
 import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
+import { ToastConfig } from "buefy/types/components";
 
 // entity
 import { Reservation } from "@/entity/reservation";
@@ -7,12 +8,35 @@ import { Reservation } from "@/entity/reservation";
 // store
 import {
   FETCH,
-  FETCH_ALL_RESERVED_TIMEZONES,
+  FETCH_BUSINESS_DATE_AFTER_TODAY,
   GET_BY_ID,
   SET_RESERVATION_DATE,
   SET_RESERVATION_TIMEZONE
 } from "@/store/constant";
 
 export default Vue.extend({
-  template: "<reservation-all-reserved-form/>"
+  template: "<reservation-all-reserved-form/>",
+  computed: {
+    ...mapState("businessDay", ["businessDays"]),
+    ...mapState("timezone", ["timezones"])
+  },
+  methods: {
+    ...mapActions("businessDay", [FETCH_BUSINESS_DATE_AFTER_TODAY]),
+    ...mapActions("timezone", [FETCH])
+  },
+  mounted() {
+    const promises = [this.fetchBusinessDateAfterToday(), this.fetch()];
+
+    Promise.all(promises)
+      .then(() => {
+        this.$emit("data-loaded");
+      })
+      .catch(() => {
+        const toastConfig: ToastConfig = {
+          message: "データの初期化に失敗しました。",
+          type: "is-danger"
+        };
+        this.$buefy.toast.open(toastConfig);
+      });
+  }
 });

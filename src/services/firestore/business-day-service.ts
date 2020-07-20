17aeae68f144947e0ee4d0aa86a4f6e80985a229
Kenta Yamada <knt.yamada.800@gamil.com>
2020-07-20 09:13:45 +0900

    #156 [営業日設定] 営業日作成時に予約可能時間を設定できるようにする

--- src/services/firestore/business-day-service.ts
+++ src/services/firestore/business-day-service.ts
@@ -1,25 +1,38 @@
 import firebase from "@/plugins/firebase";
 import _ from "lodash";
 import moment from "moment";
+
 import { BusinessDay } from "@/entity/business-day";
+import { SelectableTimezone } from "@/entity/selectable-timezone";
 
 export class BusinessDayService {
   private readonly COLLECTION_NAME: string = "business_days";
 
-  add(businessDay: BusinessDay) {
-    if (_.isNil(businessDay)) {
+  async add(businessDay: BusinessDay) {
+    const collection = firebase.firestore().collection(this.COLLECTION_NAME);
+    const businessDays = await collection.where("business_date", "==", businessDay.business_date).get();
+
+    if (!businessDays.empty) {
       return Promise.reject();
     }
 
-    // todo: type safe
-    const requestBody = {
-      business_date: businessDay.business_date
-    };
+    const businessDayRef = collection.doc();
+    businessDayRef.set({ business_date: businessDay.business_date });
 
-    return firebase
-      .firestore()
-      .collection(this.COLLECTION_NAME)
-      .add(requestBody);
+    const batch = firebase.firestore().batch();
+    _.each(businessDay.timezones, (timezone: SelectableTimezone) => {
+      const timezoneRef = businessDayRef.collection("timezones").doc();
+
+      batch.set(timezoneRef, {
+        start_time: timezone.start_time,
+        end_time: timezone.end_time,
+        selected: timezone.selected
+      });
+    });
+
+    batch.commit();
+
+    return businessDayRef;
   }
 
   edit(businessDay: BusinessDay) {

534249d1cfc2e51512089044c8699dba279dca6d
Kenta Yamada <knt.yamada.800@gamil.com>
2020-07-31 21:47:27 +0900

    modify parameters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   root: true,
-
-  env: {
-    node: true
-  },
-
+  env: { node: true },
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
@@ -12,27 +8,25 @@ module.exports = {
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
   ],
-
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-
+  parserOptions: { ecmaVersion: 2020 },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "off" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "off" : "off",
-    camelcase: "off",
-    "@typescript-eslint/camelcase": ["error", { properties: "never" }]
+    "comma-dangle": ["error", "never"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-unused-vars": ["off"],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      { selector: "default", format: ["camelCase"] },
+      { selector: "class", format: ["PascalCase"] },
+      { selector: "interface", format: ["PascalCase"] },
+      { selector: "typeAlias", format: ["PascalCase"] },
+      { selector: "property", format: null },
+      { selector: "variable", format: null },
+      { selector: "enum", format: null },
+      { selector: "enumMember", format: null },
+      { selector: "method", format: ["camelCase"], leadingUnderscore: "allow", trailingUnderscore: "allow" }
+    ]
   },
-
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+  overrides: [{ files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"], env: { jest: true } }]
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,27 @@
+/**
+ * Prettier config
+ * @see https://prettier.io/docs/en/options.html
+ */
 module.exports = {
-    printWidth: 120
-}
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  parser: undefined,
+  filepath: undefined,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: true,
+  endOfLine: "lf"
+};

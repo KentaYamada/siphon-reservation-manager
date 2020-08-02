69f5800dc1f13b052b6136c567d19120a07c8539
Kenta Yamada <knt.yamada.800@gamil.com>
2020-08-02 20:42:11 +0900

    #156 [予約一覧] 予約日、予約時間の選択肢をフィルターを使って描画する

--- src/components/reservations/search/script.ts
+++ src/components/reservations/search/script.ts
@@ -2,7 +2,9 @@ import Vue from "vue";
 import { mapActions, mapGetters, mapState } from "vuex";
 import { SelectableTimezone } from "@/entity/selectable-timezone";
 import { ReservationSearchOption } from "@/entity/reservation-search-option";
-import { FETCH, FETCH_BUSINESS_DATE_AFTER_TODAY, GET_SELECTABLE_TIMEZONES } from "@/store/constant";
+import { formatDateJp } from "@/filters/format-date-jp";
+import { timePeriod } from "@/filters/time-period";
+import { FETCH_BUSINESS_DATE_AFTER_TODAY, GET_SELECTABLE_TIMEZONES } from "@/store/constant";
 
 export default Vue.extend({
   template: "<reservation-search-form/>",
@@ -28,6 +30,10 @@ export default Vue.extend({
     handleSearch(): void {
       this.$emit("search", this.option);
     }
+  },
+  filters: {
+    formatDateJp,
+    timePeriod
   },
   data() {
     const option: ReservationSearchOption = {

dc6c08aa85d4266ee9b45b36a8c71ba3b7f64369
Kenta Yamada <knt.yamada.800@gmail.com>
2020-10-02 21:14:44 +0900

    予約者情報をコンポーネント化(#136)

--- src/components/reservations/dialog/address/script.ts
+++ src/components/reservations/dialog/address/script.ts
@@ -3,7 +3,7 @@ import { ReservationListSeat } from "@/entity/reservation-list-seat";
 
 export default Vue.extend({
   props: {
-    seat: {
+    reserver: {
       required: true,
       type: Object as PropType<ReservationListSeat>
     }

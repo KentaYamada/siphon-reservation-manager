ee650e95a6cc1a4f6966091a32eec7ba94bdf048
Kenta Yamada <knt.yamada.800@gmail.com>
2020-10-04 11:47:42 +0900

    [営業日設定] refactoring test codes (#151)

--- tests/unit/firestore/security-rules/business-day.spec.ts
+++ tests/unit/firestore/security-rules/business-day.spec.ts
@@ -1,8 +1,11 @@
 import * as firebase from "@firebase/testing";
 
 const MY_PROJECT_ID = "siphon-reservation-manager-dev";
+const getBusinessDays = () => {
+  return firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore().collection("business_days");
+};
 
-describe("business day security rules tests", () => {
+describe("business day & timezones security rules tests", () => {
   afterAll(async () => {
     await Promise.all(firebase.apps().map(app => app.delete()));
   });
@@ -12,14 +15,12 @@ describe("business day security rules tests", () => {
   });
 
   it("read items in the business days", async () => {
-    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-    const doc = db.collection("business_days").doc("testId");
-    await firebase.assertSucceeds(doc.get());
+    const businessDays = getBusinessDays();
+    await firebase.assertSucceeds(businessDays.get());
   });
 
   it("Can write document in the business day", async () => {
-    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-    const doc = db.collection("business_days").doc();
+    const doc = getBusinessDays().doc();
     await firebase.assertSucceeds(
       doc.set({
         business_date: new Date(),
@@ -29,9 +30,19 @@ describe("business day security rules tests", () => {
     );
   });
 
+  it("Can write document in the business day when published_datetime is null", async () => {
+    const doc = getBusinessDays().doc();
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: null,
+        is_pause: true
+      })
+    );
+  });
+
   it("Can't write document when request data size less than 3", async () => {
-    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-    const doc = db.collection("business_days").doc();
+    const doc = getBusinessDays().doc();
     await firebase.assertFails(
       doc.set({
         published_datetime: new Date(),
@@ -41,8 +52,7 @@ describe("business day security rules tests", () => {
   });
 
   it("Can't write document when request data size more than 3", async () => {
-    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-    const doc = db.collection("business_days").doc();
+    const doc = getBusinessDays().doc();
     await firebase.assertFails(
       doc.set({
         business_date: new Date(),
@@ -54,8 +64,7 @@ describe("business day security rules tests", () => {
   });
 
   it("Can't write document when request is no businass_date", async () => {
-    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-    const doc = db.collection("business_days").doc();
+    const doc = getBusinessDays().doc();
     await firebase.assertFails(
       doc.set({
         hoge: "hoge",
@@ -66,8 +75,7 @@ describe("business day security rules tests", () => {
   });
 
   it("Can't write document when request is no published_datetime", async () => {
-    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-    const doc = db.collection("business_days").doc();
+    const doc = getBusinessDays().doc();
     await firebase.assertFails(
       doc.set({
         business_date: new Date(),
@@ -78,8 +86,7 @@ describe("business day security rules tests", () => {
   });
 
   it("Can't write document when business_date is null", async () => {
-    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-    const doc = db.collection("business_days").doc();
+    const doc = getBusinessDays().doc();
     await firebase.assertFails(
       doc.set({
         business_date: new Date(),
@@ -90,8 +97,7 @@ describe("business day security rules tests", () => {
   });
 
   it("Can't write document when business_date is null", async () => {
-    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-    const doc = db.collection("business_days").doc();
+    const doc = getBusinessDays().doc();
     await firebase.assertFails(
       doc.set({
         business_date: null,
@@ -102,13 +108,210 @@ describe("business day security rules tests", () => {
   });
 
   it("Can't write document when is_pause is null", async () => {
-    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-    const doc = db.collection("business_days").doc();
+    const doc = getBusinessDays().doc();
     await firebase.assertFails(
       doc.set({
         business_date: new Date(),
         published_datetime: new Date(),
         is_pause: null
+      })
+    );
+  });
+
+  it("Can write timezones in the business day", async () => {
+    const businessDays = getBusinessDays();
+    const doc = businessDays.doc();
+    const subDoc = doc.collection("timezones").doc();
+
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: new Date(),
+        is_pause: true
+      })
+    );
+
+    await firebase.assertSucceeds(
+      subDoc.set({
+        start_time: new Date(),
+        end_time: new Date(),
+        selected: true
+      })
+    );
+  });
+
+  it("Can't write timezones in the business day when request size less than 3", async () => {
+    const businessDays = getBusinessDays();
+    const doc = businessDays.doc();
+    const subDoc = doc.collection("timezones").doc();
+
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: new Date(),
+        is_pause: true
+      })
+    );
+
+    await firebase.assertFails(
+      subDoc.set({
+        start_time: new Date(),
+        end_time: new Date()
+      })
+    );
+  });
+
+  it("Can't write timezones in the business day when request size more than 3", async () => {
+    const businessDays = getBusinessDays();
+    const doc = businessDays.doc();
+    const subDoc = doc.collection("timezones").doc();
+
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: new Date(),
+        is_pause: true
+      })
+    );
+
+    await firebase.assertFails(
+      subDoc.set({
+        start_time: new Date(),
+        end_time: new Date(),
+        selected: true,
+        hoge: "hoge"
+      })
+    );
+  });
+
+  it("Can't write timezones when request is not contain start_time", async () => {
+    const businessDays = getBusinessDays();
+    const doc = businessDays.doc();
+    const subDoc = doc.collection("timezones").doc();
+
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: new Date(),
+        is_pause: true
+      })
+    );
+
+    await firebase.assertFails(
+      subDoc.set({
+        hoge: "hoge",
+        end_time: new Date(),
+        selected: true
+      })
+    );
+  });
+
+  it("Can't write timezones when request is not contain end_time", async () => {
+    const businessDays = getBusinessDays();
+    const doc = businessDays.doc();
+    const subDoc = doc.collection("timezones").doc();
+
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: new Date(),
+        is_pause: true
+      })
+    );
+
+    await firebase.assertFails(
+      subDoc.set({
+        start_time: new Date(),
+        hoge: "hoge",
+        selected: true
+      })
+    );
+  });
+
+  it("Can't write timezones when request is not contain selected", async () => {
+    const businessDays = getBusinessDays();
+    const doc = businessDays.doc();
+    const subDoc = doc.collection("timezones").doc();
+
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: new Date(),
+        is_pause: true
+      })
+    );
+
+    await firebase.assertFails(
+      subDoc.set({
+        start_time: new Date(),
+        end_time: new Date(),
+        hoge: "hoge"
+      })
+    );
+  });
+
+  it("Can't write timezones when start_time is null", async () => {
+    const businessDays = getBusinessDays();
+    const doc = businessDays.doc();
+    const subDoc = doc.collection("timezones").doc();
+
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: new Date(),
+        is_pause: true
+      })
+    );
+
+    await firebase.assertFails(
+      subDoc.set({
+        start_time: null,
+        end_time: new Date(),
+        selected: true
+      })
+    );
+  });
+
+  it("Can't write timezones when end_time is null", async () => {
+    const businessDays = getBusinessDays();
+    const doc = businessDays.doc();
+    const subDoc = doc.collection("timezones").doc();
+
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: new Date(),
+        is_pause: true
+      })
+    );
+
+    await firebase.assertFails(
+      subDoc.set({
+        start_time: new Date(),
+        end_time: null,
+        selected: true
+      })
+    );
+  });
+
+  it("Can't write timezones when end_time is null", async () => {
+    const businessDays = getBusinessDays();
+    const doc = businessDays.doc();
+    const subDoc = doc.collection("timezones").doc();
+
+    await firebase.assertSucceeds(
+      doc.set({
+        business_date: new Date(),
+        published_datetime: new Date(),
+        is_pause: true
+      })
+    );
+
+    await firebase.assertFails(
+      subDoc.set({
+        start_time: new Date(),
+        end_time: new Date(),
+        selected: null
       })
     );
   });

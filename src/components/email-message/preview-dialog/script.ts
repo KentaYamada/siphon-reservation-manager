862a41594da044c365cd150bc20c4b706e98751d
Kenta Yamada <knt.yamada.800@gmail.com>
2020-10-11 12:12:43 +0900

    [EmailMessage] 改行、文字置換対応(#208)

--- src/components/email-message/preview-dialog/script.ts
+++ src/components/email-message/preview-dialog/script.ts
@@ -1,11 +1,20 @@
 import Vue, { PropType } from "vue";
 import { EmailMessage } from "@/entity/email-message";
+import { nl2br } from "@/filters/nl2br";
 
 export default Vue.extend({
   props: {
     message: {
       required: true,
       type: Object as PropType<EmailMessage>
+    }
+  },
+  computed: {
+    body(): string {
+      const replacedMessage = this.message.body
+        .replace("{予約者名}", "Gamoyon花子")
+        .replace("{予約サイトURL}", `${location.origin}/reservations`);
+      return nl2br(replacedMessage);
     }
   }
 });
--- src/services/firestore/email-message-service.ts
+++ src/services/firestore/email-message-service.ts
@@ -1,10 +1,15 @@
 import { from, Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { ApiResponse } from "@/entity/api-response";
 import { EmailMessage } from "@/entity/email-message";
 import firebase from "@/plugins/firebase";
 
 export class EmailMessageService {
   static readonly COLLECTION_NAME = "email_messages";
+
+  private static _getCollection() {
+    return firebase.firestore().collection(EmailMessageService.COLLECTION_NAME);
+  }
 
   static fetch(): Observable<Array<EmailMessage>> {
     return new Observable(subscriber => {
@@ -42,9 +47,27 @@ export class EmailMessageService {
         theme: "is-success",
         category: "予約完了メッセージ",
         subject: "[Cafe de GAMOYON] 予約完了しました",
-        body: "Thank you for reservation"
+        body: `Thank you for reservation
+            {予約者名}
+            {予約サイトURL}`
       };
       subscriber.next(message);
     });
+  }
+
+  static edit(payload: EmailMessage): Observable<ApiResponse<void>> {
+    const docRef = EmailMessageService._getCollection().doc(payload.id);
+    const data: firebase.firestore.DocumentData = {
+      subject: payload.subject,
+      body: payload.body
+    };
+
+    return from(docRef.update(data)).pipe(
+      map(() => {
+        return {
+          message: "変更しました"
+        } as ApiResponse<void>;
+      })
+    );
   }
 }

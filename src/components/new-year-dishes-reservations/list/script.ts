f9e531e37c7660285e3f1168b25dde625c9d457a
Kenta Yamada <knt.yamada.800@gmail.com>
2020-11-03 14:39:18 +0900

    おせち予約一覧コンポーネント作成(#223)

---
+++ src/components/new-year-dishes-reservations/list/script.ts
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import NewYearDishesReservationListItem from "@/components/new-year-dishes-reservations/list-item/NewYearDishesReservationListItem.vue";
+
+export default Vue.extend({
+  template: "<new-year-dishes-reservation-list/>",
+  components: {
+    NewYearDishesReservationListItem
+  }
+});
---
+++ src/views/new-year-dishes-reservations/list/script.ts
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import NewYearDishesReservationList from "@/components/new-year-dishes-reservations/list/NewYearDishesReservationList.vue";
+
+export default Vue.extend({
+  components: {
+    NewYearDishesReservationList
+  }
+});

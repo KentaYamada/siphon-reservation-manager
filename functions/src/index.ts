1987e584d7a6c58115af5c5119936c776e74cbb8
Kenta Yamada <knt.yamada.800@gmail.com>
2020-11-15 12:25:44 +0900

    おせち予約メール配信(#223)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,6 +15,23 @@ const mailTransport = nodemailer.createTransport({
   }
 });
 
+const getNewYearDishesReservedMessage = (reserverName: string): string => {
+  return `${reserverName} 様
+Cafe de Gamoyonです
+スイーツおせちのご予約ありがとうございます！
+
+おせちの数量変更または、キャンセルの際は、このメールへご返信ください
+
+Cafe de Gamoyon
+大阪府大阪市城東区今福東2-9-19
+https://goo.gl/maps/X2NpTBBGJBamfPgw6
+
+最新情報をSNSでチェック！
+Instagram: https://www.instagram.com/cafedegamoyon/?hl=ja
+Facebook: https://www.facebook.com/gamoyon4204/
+    `;
+};
+
 exports.sendMail = functions.https.onRequest((request, response: functions.Response) => {
   return cors(request, response, () => {
     const message = {
@@ -40,3 +57,23 @@ exports.sendMail = functions.https.onRequest((request, response: functions.Respo
       });
   });
 });
+
+exports.onCreateNewYearDishesReservation = functions.firestore
+  .document("new_year_dishes_reservations/{id}")
+  .onCreate(snap => {
+    const data = snap.data();
+    const message = getNewYearDishesReservedMessage(data.reserver_name);
+    const sendData = {
+      from: `Cafe de Gamoyon <${config.gmail.email}>`,
+      to: data.mail,
+      bcc: config.gmail.email,
+      subject: "[Cafe de Gamoyon] スイーツおせち予約完了のお知らせ",
+      text: message
+    };
+
+    console.log(sendData);
+
+    return mailTransport.sendMail(sendData)
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+  });

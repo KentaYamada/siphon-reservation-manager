e9ccb0aa4877fa1d9e62e62b84f7bf4c4adc18c0
Kenta Yamada <knt.yamada.800@gmail.com>
2020-11-22 01:12:30 +0900

    [予約] 旧予約保存メソッド削除(#224)

--- src/services/firestore/reservation-service.ts
+++ src/services/firestore/reservation-service.ts
@@ -7,11 +7,10 @@ import { ReservationSeat } from "@/entity/reservation-seat";
 import firebase from "@/plugins/firebase";
 
 export class ReservationService {
-  private readonly COLLECTION_NAME: string = "reservations";
-  private readonly SUB_COLLECTION_NAME: string = "reservation_seats";
+  private static readonly COLLECTION_NAME: string = "reservations";
 
   private static _getCollection() {
-    return firebase.firestore().collection("reservations");
+    return firebase.firestore().collection(ReservationService.COLLECTION_NAME);
   }
 
   static save(payload: Reservation): Observable<firebase.firestore.DocumentSnapshot> {
@@ -20,7 +19,7 @@ export class ReservationService {
     }
 
     const db = firebase.firestore();
-    const collection = db.collection("reservations");
+    const collection = db.collection(ReservationService.COLLECTION_NAME);
     const transaction$ = db.runTransaction(async transaction => {
       // todo: 座席引当
       const docRef = payload.id ? collection.doc(payload.id) : collection.doc();
@@ -50,121 +49,6 @@ export class ReservationService {
     return from(transaction$);
   }
 
-  async save(reservation: Reservation) {
-    if (_.isNil(reservation)) {
-      return Promise.reject({
-        message: "予約処理に失敗しました"
-      });
-    }
-
-    const hasReserved = await this._existReservedSeats(reservation);
-    if (hasReserved) {
-      return Promise.reject({
-        message: "選択した座席はすでに予約済です。お手数ですが再度選択してください。",
-        refetch_seats: true
-      });
-    }
-
-    const db = firebase.firestore();
-    const transaction = db.runTransaction(async transaction => {
-      const reservationsRef = db.collection(this.COLLECTION_NAME);
-      const reservationRef = reservation.id ? reservationsRef.doc(reservation.id) : reservationsRef.doc();
-      const existData = await transaction.get(reservationRef);
-      const reservationData: firebase.firestore.DocumentData = {
-        reservation_date: reservation.reservation_date,
-        reservation_date_id: reservation.reservation_date_id,
-        reservation_start_time: reservation.reservation_start_time,
-        reservation_end_time: reservation.reservation_end_time,
-        reservation_time_id: reservation.reservation_time_id,
-        reserver_name: reservation.reserver_name,
-        number_of_reservations: reservation.number_of_reservations,
-        tel: reservation.tel,
-        mail: reservation.mail,
-        comment: reservation.comment
-      };
-
-      if (existData.exists) {
-        transaction.update(reservationRef, reservationData);
-
-        const reservedSeatsRef = await db
-          .collection(this.SUB_COLLECTION_NAME)
-          .where("reservation_id", "==", reservationRef.id)
-          .get();
-        reservedSeatsRef.forEach(doc => {
-          transaction.update(doc.ref, { reservation_id: null, is_reserved: false });
-        });
-      } else {
-        transaction.set(reservationRef, reservationData);
-      }
-
-      const reservationSeatsRef = await db
-        .collection(this.SUB_COLLECTION_NAME)
-        .where("reservation_date", "==", reservation.reservation_date)
-        .where("reservation_time_id", "==", reservation.reservation_time_id)
-        .get();
-
-      if (reservationSeatsRef.empty) {
-        _.each(reservation.reservation_seats, (seat: ReservationSeat) => {
-          const reservationSeatRef = db.collection(this.SUB_COLLECTION_NAME).doc();
-          const seatData: firebase.firestore.DocumentData = {
-            seat_no: seat.seat_no,
-            is_reserved: false,
-            reservation_id: null,
-            reservation_date: reservation.reservation_date,
-            reservation_date_id: reservation.reservation_date_id,
-            reservation_start_time: reservation.reservation_start_time,
-            reservation_end_time: reservation.reservation_end_time,
-            reservation_time_id: reservation.reservation_time_id
-          };
-
-          if (seat.is_selected) {
-            seatData.reservation_id = reservationRef.id;
-            seatData.is_reserved = true;
-          }
-
-          transaction.set(reservationSeatRef, seatData);
-        });
-      } else {
-        _.each(reservation.reservation_seats, (seat: ReservationSeat) => {
-          const seatRef = _.find(reservationSeatsRef.docs, doc => {
-            return seat.seat_no === doc.data()?.seat_no;
-          });
-
-          if (_.isNil(seatRef)) {
-            return Promise.reject();
-          }
-
-          const seatData: firebase.firestore.DocumentData = {
-            seat_no: seat.seat_no,
-            reservation_date: reservation.reservation_date,
-            reservation_date_id: reservation.reservation_date_id,
-            reservation_time_id: reservation.reservation_time_id,
-            reservation_start_time: reservation.reservation_start_time,
-            reservation_end_time: reservation.reservation_end_time
-          };
-
-          if (_.isEmpty(seatRef.data().reservation_id) && seat.is_selected) {
-            // 予約座席を追加
-            seatData.reservation_id = reservationRef.id;
-            seatData.is_reserved = true;
-          }
-
-          if (seatRef.data()?.reservation_id === reservationRef.id) {
-            // 自身の予約座席
-            seatData.is_reserved = seat.is_selected;
-            seatData.reservation_id = seat.is_selected ? seat.reservation_id : null;
-          }
-
-          transaction.update(seatRef.ref, seatData);
-        });
-      }
-
-      return reservationRef.id;
-    });
-
-    return transaction;
-  }
-
   static cancel(id: string): Observable<void> {
     if(!id) {
       throwError("不正なリクエストです。");
@@ -182,7 +66,7 @@ export class ReservationService {
 
     let query = firebase
       .firestore()
-      .collection(this.COLLECTION_NAME)
+      .collection(ReservationService.COLLECTION_NAME)
       .where("reservation_date_id", "==", option.reservation_date_id);
 
     if (!_.isEmpty(option.reservation_time_id)) {
@@ -218,33 +102,5 @@ export class ReservationService {
         } as Reservation;
       })
     );
-  }
-
-  private async _existReservedSeats(reservation: Reservation): Promise<boolean> {
-    const selectedSeatNo: Array<number> = [];
-
-    _.each(reservation.reservation_seats, (seat: ReservationSeat) => {
-      if (seat.is_selected) {
-        selectedSeatNo.push(seat.seat_no);
-      }
-    });
-
-    const reservationSeatsRef = await firebase
-      .firestore()
-      .collection(this.SUB_COLLECTION_NAME)
-      .where("reservation_date_id", "==", reservation.reservation_date_id)
-      .where("reservation_time_id", "==", reservation.reservation_time_id)
-      .where("is_reserved", "==", true)
-      .get();
-
-    let hasReserved = false;
-    _.each(reservationSeatsRef.docs, doc => {
-      if (doc.data()?.reservation_id !== reservation.id && _.includes(selectedSeatNo, doc.data()?.seat_no)) {
-        hasReserved = true;
-        return false;
-      }
-    });
-
-    return hasReserved;
   }
 }

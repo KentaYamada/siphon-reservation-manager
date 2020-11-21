fa8efdec63e0894237ee51d53c8973d93216558f
Kenta Yamada <knt.yamada.800@gmail.com>
2020-11-22 01:07:19 +0900

    [予約フォーム] 不要なemitを削除(#224)

--- src/views/reservations/entry/script.ts
+++ src/views/reservations/entry/script.ts
@@ -1,12 +1,10 @@
 import Vue from "vue";
 import _ from "lodash";
 import moment from "moment";
-import { mapState } from "vuex";
 import { BNoticeConfig } from "buefy/types/components";
 import { forkJoin } from "rxjs";
 import ReservationForm from "@/components/reservations/form/ReservationForm.vue";
 import { EMAIL_MESSAGE_TEMPLATES } from "@/entity/email";
-import { NewYearDishesReservation } from "@/entity/new-year-dishes-reservation";
 import { NewYearDishesSetting } from "@/entity/new-year-dishes-setting";
 import { NewYearDishesReservationService } from "@/services/firestore/new-year-dishes-reservation-service";
 import { NewYearDishesSettingService } from "@/services/firestore/new-year-dishes-setting-service";
@@ -17,8 +15,6 @@ export default Vue.extend({
     ReservationForm
   },
   computed: {
-    ...mapState("reservation", ["reservation"]),
-
     isAccessible(): boolean {
       if (this.setting.is_pause) {
         return false;
@@ -36,16 +32,7 @@ export default Vue.extend({
     }
   },
   methods: {
-    handleInitializing(): void {
-      this.isProgressing = true;
-    },
-
-    handleInitialized(): void {
-      this.isProgressing = false;
-    },
-
-    handleInitializeFailure(): void {
-      // todo: redirect 503 page
+    handleInitializeFailed() {
       const toastConfig: BNoticeConfig = {
         message: "データの初期化に失敗しました",
         type: "is-danger"
@@ -63,46 +50,42 @@ export default Vue.extend({
       this.$buefy.toast.open(toastConfig);
     },
 
-    handleSaving(): void {
-      this.isProgressing = true;
-    },
-
-    handleSaveSuceeded(reservationId: string): void {
+    handleSaveSucceeded(id: string) {
       const toastConfig: BNoticeConfig = {
         message: "予約しました",
         type: "is-success"
       };
 
-      this.isProgressing = false;
       this.$buefy.toast.open(toastConfig);
-      this._sendEmail(reservationId);
-      this.$router.push({ name: "reserved-message", params: { id: reservationId } });
+
+      // todo: trigger
+      this._sendEmail(id);
+      this.$router.push({ name: "reserved-message", params: { id: id } });
     },
 
-    handleSaveFailure(error: any): void {
-      let message = "予約することができませんでした";
-      if (error.message) {
-        message = error.message;
-      }
-
-      const toastConfig: BNoticeConfig = {
-        message: message,
+    handleSaveFailed() {
+      const config: BNoticeConfig = {
+        message: "予約することができませんでした",
         type: "is-danger"
       };
 
-      this.isProgressing = false;
-      this.$buefy.toast.open(toastConfig);
+      this.$buefy.toast.open(config);
     },
 
-    handleValidationFailure(): void {
-      const toastConfig: BNoticeConfig = {
+    handleUpdateProgress(isProgress: boolean) {
+      this.isProgressing = isProgress;
+    },
+
+    handleValidationFailed() {
+      const config: BNoticeConfig = {
         message: "入力内容に誤りがあります。エラーメッセージを確認してください。",
         type: "is-danger"
       };
 
       this.isProgressing = false;
-      this.$buefy.toast.open(toastConfig);
+      this.$buefy.toast.open(config);
     },
+
 
     _sendEmail(id: string): void {
       const href = this.$router.resolve({
@@ -113,7 +96,7 @@ export default Vue.extend({
       }).href;
       const redirectUrl = `${location.origin}${href}`;
       // todo: cloud functions triggerを使ってメール配信する
-      sendEmail(this.reservation, id, redirectUrl, EMAIL_MESSAGE_TEMPLATES.CREATED);
+      // sendEmail(this.reservation, id, redirectUrl, EMAIL_MESSAGE_TEMPLATES.CREATED);
     }
   },
   created() {

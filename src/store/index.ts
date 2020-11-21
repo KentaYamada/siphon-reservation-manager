8dbb0dc2c88b6c4a5b80832c3c7d327a7c69d86d
Kenta Yamada <knt.yamada.800@gmail.com>
2020-11-21 14:47:29 +0900

    Remove new year dishes setting store

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,6 @@ import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import auth from "@/store/auth";
 import businessDay from "@/store/business-day";
-import newYearDishesSetting from "@/store/new-year-dishes-setting";
 import reservation from "@/store/reservation";
 import reservationList from "@/store/reservation-list";
 import timezone from "@/store/timezone";
@@ -18,7 +17,6 @@ export default new Vuex.Store({
   modules: {
     auth,
     businessDay,
-    newYearDishesSetting,
     reservation,
     reservationList,
     timezone

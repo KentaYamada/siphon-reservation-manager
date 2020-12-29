90264fbef892e1f4804e3b6b2fb2d0c05063276b
Kenta Yamada <knt.yamada.800@gamil.com>
2020-12-29 17:43:02 +0900

    [予約キャンセル履歴] firestoreからの取得部分仮実装 (#157)

---
+++ src/entity/reservation-cancel-log-search-option.ts
@@ -0,0 +1,7 @@
+/**
+ * Reservation cancel log search option entity
+ */
+export interface ReservationCancelLogSearchOption {
+  reservation_date_id: string;
+  reservation_time_id: string;
+}
--- src/services/firestore/reservation-cancel-log-service.ts
+++ src/services/firestore/reservation-cancel-log-service.ts
@@ -1,10 +1,27 @@
 import { Observable, from } from "rxjs";
+import { isEmpty } from "lodash";
 import { ReservationCancelLog } from "@/entity/reservation-cancel-log";
 import { ReservationCancelLogSearchOption } from "@/entity/reservation-cancel-log-search-option";
 import firebase from "@/plugins/firebase";
 
 export class ReservationCancelLogService {
+  // private static getCollection() {
+  //   return firebase.firestore().collection("reservation_cancel_logs");
+  // }
+
   static fetch(params: ReservationCancelLogSearchOption): Observable<Array<ReservationCancelLog>> {
+    // let query = ReservationCancelLogService.getCollection().where(
+    //   "reservation_date_id",
+    //   "==",
+    //   params.reservation_date_id
+    // );
+
+    // if (!isEmpty(params.reservation_time_id)) {
+    //   query = query.where("reservation_time_id", "==", params.reservation_time_id);
+    // }
+
+    // return from(query.get());
+
     return new Observable(subscriber => {
       const items: Array<ReservationCancelLog> = [
         {

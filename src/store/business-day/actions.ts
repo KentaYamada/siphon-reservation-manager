0482e7c563ed99d7860681ed6a315372f931016f
Kenta Yamada <knt.yamada.800@gmail.com>
2021-01-02 22:48:14 +0900

    [タイムテーブル] 時間帯できれいに並べられるように日付を固定で合わせてソートする(#247)

--- src/store/business-day/actions.ts
+++ src/store/business-day/actions.ts
@@ -54,14 +54,26 @@ const actions: ActionTree<BusinessDayState, RootState> = {
         const timezonesRef = await doc.ref.collection(service.subCollectionName).where("selected", "==", true).get();
         businessDay.timezones = _.chain(timezonesRef.docs)
           .map(doc => {
-            return {
+            const startTime = moment(doc.data()?.start_time?.toDate()).set({
+              year: 2020,
+              month: 0,
+              date: 1
+            });
+            const endTime = moment(doc.data()?.end_time?.toDate()).set({
+              year: 2020,
+              month: 0,
+              date: 1
+            });
+            const timezone: SelectableTimezone = {
               id: doc.id,
-              start_time: doc.data().start_time.toDate(),
-              end_time: doc.data().end_time.toDate(),
+              start_time: startTime.toDate(),
+              end_time: endTime.toDate(),
               selected: doc.data().selected
-            } as SelectableTimezone;
+            };
+
+            return timezone;
           })
-          .sortBy((t: SelectableTimezone) => t.start_time.getHours())
+          .orderBy(["start_time", "end_time"], ["asc", "asc"])
           .value();
 
         businessDays.push(businessDay);
